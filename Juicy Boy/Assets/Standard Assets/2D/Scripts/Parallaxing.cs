f624c960ec8962a070cd535daa6048d4f5bf4c3c
johnny <johnnytn>
2015-09-01 21:08:47 -0300

    Juicy_Boy

---
+++ Juicy Boy/Assets/Standard Assets/2D/Scripts/Parallaxing.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Parallaxing : MonoBehaviour
+{
+	public List<Transform> backgroundList = new List<Transform>();
+	//public Transform[] backgrounds; 	//Array (list) of all the back and foregrounds to be parallaxed
+	private float[] paralaxSaceles; 	// the proportion of the camera's movement to move the backgrounds by
+	public float smoothing = 1; 		// How smooth the parallax is going to be. Make sure to be above 0.
+
+	private Transform cam;				// Reference to the main camares transform
+	private Vector3 previousCamPos;		// The position of the camera in the previous frame
+
+	// Called before the Start()
+	void Awake ()
+	{
+		// Set up camera reference
+		cam = Camera.main.transform;
+
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+
+		// Storing previous cam position
+		previousCamPos = cam.position;
+		paralaxSaceles = new float[backgroundList.Count];
+
+		// asigning coresponding parallaxScales
+		for (int i = 0; i < backgroundList.Count; i++) {
+
+			paralaxSaceles [i] = backgroundList[i].position.z * -1;
+
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		for (int i = 0; i < backgroundList.Count; i++) {
+			// The parallax is the opposite of the camera movement
+			float parallax = (previousCamPos.x - cam.position.x) * paralaxSaceles[i];
+
+			// Set a target x position which is the current position plus the parallax
+			float backgroundTargetPosX = backgroundList[i].position.x + parallax;
+
+			// Create a target position which is the background's current position with its target position
+			Vector3 backgroundTargetPos = new Vector3 (backgroundTargetPosX,backgroundList[i].position.y,
+			                                           backgroundList[i].position.z);
+
+			backgroundList[i].position = Vector3.Lerp (backgroundList[i].position, 
+			                                           backgroundTargetPos, smoothing * Time.deltaTime);			
+		}
+
+		previousCamPos = cam.position;
+	
+	}
+}
---
+++ Juicy Boy/Assets/Standard Assets/2D/Scripts/Tiling.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(SpriteRenderer))]
+
+public class Tiling : MonoBehaviour {
+
+	//private Parallaxing parallaxing;
+
+	public int offSetX = 2;
+
+	// Used to check if we need to instantiate 
+	public bool hasARightBuddy = false;
+	public bool hasALeftBuddy = false;
+
+	public bool reverseScale = false;
+
+	public float spriteWidth = 0f;
+	private Camera cam;
+	private Transform myTransform;
+
+	void Awake() {
+
+		//parallaxing = GetComponent<Parallaxing>();
+		cam = Camera.main;
+		myTransform = transform;
+	}
+
+	// Use this for initialization
+	void Start () {
+		SpriteRenderer sRenderer = GetComponent<SpriteRenderer>();
+		spriteWidth = sRenderer.sprite.bounds.size.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!hasALeftBuddy || !hasARightBuddy) {
+			float camHorizontalExtend = cam.orthographicSize * Screen.width/Screen.height;
+
+			float edgeVisiblePosRight = (myTransform.position.x + spriteWidth/2) - camHorizontalExtend;
+			float edgeVisiblePosLeft = (myTransform.position.x - spriteWidth/2) + camHorizontalExtend;
+
+			if ((cam.transform.position.x >= edgeVisiblePosRight - offSetX) && !hasARightBuddy) {
+				makeNewBuddy(1);
+				hasARightBuddy = true;
+
+			} else if ((cam.transform.position.x <= edgeVisiblePosLeft + offSetX) && !hasALeftBuddy ) { 
+				makeNewBuddy(-1);
+				hasALeftBuddy = true;
+			}
+		}
+	
+	}
+
+	// Used to create a new Buddy on the required side
+	void makeNewBuddy(int rightOrLeft) {
+		Vector3 newPosition = new Vector3 (myTransform.position.x + spriteWidth * rightOrLeft,
+		                                   myTransform.position.y, 
+		                                   myTransform.position.z);
+
+		// Instantiating new buddy
+		Transform newBuddy = (Transform) Instantiate(myTransform, newPosition, myTransform.rotation);
+
+		// Check if it's tilable, if not reverse it
+		if(reverseScale) {
+			newBuddy.localScale = new Vector3 (newBuddy.localScale.x * -1,
+			                                   newBuddy.localScale.y,
+			                                   newBuddy.localScale.z);
+		}
+		newBuddy.parent = myTransform.parent;
+
+		if (rightOrLeft > 0) {
+			newBuddy.GetComponent<Tiling>().hasALeftBuddy = true;
+		} else {
+			newBuddy.GetComponent<Tiling>().hasARightBuddy = true;
+		}
+
+		//parallaxing.backgroundList.Add(myTransform);
+	}
+}
